3cfcc8b9f49e5ea44048b6d0ca95680a7593568e
Tycho Sci <tychosci@gmail.com>
2012-01-23 01:37:33 +0900

    Fix sort of things
    * change function's name.
    * remove `test.txt` from example prompt.
    * move piece of code that does loop over `mecab_dictionary_info_t`.

--- testflight.rs
+++ testflight.rs
@@ -3,15 +3,16 @@
 test:
 
   % rustc mecab.rc
-  % ./mecab test.txt
+  % ./mecab
 
 */
 
 fn test_pass_mecab(_mecab: mecab) {
 }
 
-fn test_mecab_new(args: [str]) {
+fn example_singlethread(args: [str]) {
     let m = mecab_new(vec::len(args), args);
+
     test_pass_mecab(m);
 
     let input = "夕焼け小焼けの赤とんぼ";
@@ -22,28 +23,29 @@ fn test_mecab_new(args: [str]) {
             std::io::print(#fmt["input: %s\n", input]);
             std::io::print(#fmt["output:\n%s", s]);
 
-            let dict = m.get_dictionary_info();
-
-            while !dict.is_end() {
-                std::io::print(#fmt["filename: %s\n", dict.get_filename()]);
-                std::io::print(#fmt["charset:  %s\n", dict.get_charset()]);
-                std::io::print(#fmt["size:     %u\n", dict.get_size()]);
-                std::io::print(#fmt["type:     %d\n", dict.get_type()]);
-                std::io::print(#fmt["lsize:    %u\n", dict.get_lsize()]);
-                std::io::print(#fmt["rsize:    %u\n", dict.get_rsize()]);
-                std::io::print(#fmt["version:  %u\n", dict.get_version()]);
-                dict.bump();
-            }
         }
         none::<str> {
             fail #fmt["Exception: %s", m.strerror()];
         }
     }
+
+    let dict = m.get_dictionary_info();
+
+    while !dict.is_end() {
+        std::io::print(#fmt["filename: %s\n", dict.get_filename()]);
+        std::io::print(#fmt["charset:  %s\n", dict.get_charset()]);
+        std::io::print(#fmt["size:     %u\n", dict.get_size()]);
+        std::io::print(#fmt["type:     %d\n", dict.get_type()]);
+        std::io::print(#fmt["lsize:    %u\n", dict.get_lsize()]);
+        std::io::print(#fmt["rsize:    %u\n", dict.get_rsize()]);
+        std::io::print(#fmt["version:  %u\n", dict.get_version()]);
+        dict.bump();
+    }
 }
 
 fn main(args: [str]) {
     alt task::try {||
-        test_mecab_new(args);
+        example_singlethread(args);
     } {
         result::ok(())  { /* do nothing */ }
         result::err(()) { sys::set_exit_status(1); }
