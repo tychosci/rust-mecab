4e49a1bf68f1c6865b1bf38687f3369d905203a6
Tycho Sci <tychosci@gmail.com>
2012-02-03 18:55:00 +0900

    Fix method "get_surface" to work correctly

--- mecab.rs
+++ mecab.rs
@@ -287,7 +287,7 @@ impl of mecab_node for *mecab_node_t {
 
         check uint::le(begin, end);
 
-        str::slice(s, begin, end)
+        str::substr(s, begin, end)
     }
 
     fn get_feature() -> str unsafe {
